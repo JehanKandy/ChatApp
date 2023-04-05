9433b639faa0608e71eadb241d1426dc3fbbd659
JehanKandy <jehankandy@gmail.com>
2023-04-05 12:16:31 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -8,8 +8,43 @@
     function reg_user($username, $email, $pass, $cpass){
         $con = Connection();
 
-        if(empty($username)){
-            return "Empty Username";
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            return  "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+                    <strong>Email Error</strong> &nbsp; Check Again Your Email
+                    <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                    <span aria-hidden='true'>&times;</span>
+                    </button>
+                </div>";
+        }
+        elseif(empty($username)){
+            return "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+                        <strong>Username : </strong> Connot be Empty...!
+                        <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                </div>";
+        }
+        elseif(empty($email)){
+            return "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+                        <strong>Username : </strong> Connot be Empty...!
+                        <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                </div>";
+        }
+        elseif(empty($pass)){
+            return "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+                        <strong>Username : </strong> Connot be Empty...!
+                        <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                </div>";
+        }
+        elseif(empty($cpass)){
+            return "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+                        <strong>Username : </strong> Connot be Empty...!
+                        <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                </div>";
+        }
+        elseif(empty($username)){
+            return "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+                        <strong>Username : </strong> Connot be Empty...!
+                        <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                </div>";
         }
         
     }
