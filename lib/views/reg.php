8df4dd4061e2a7fc209f3ba10b6f6c3d14ab016b
JehanKandy <jehankandy@gmail.com>
2023-04-05 09:18:41 +0530

    Update reg.php

--- lib/views/reg.php
+++ lib/views/reg.php
@@ -12,14 +12,20 @@
                 <label for="username">Username : </label> <br>
                 <input type="text" name="username" id="username" placeholder="Username"> <br>
 
+                <label for="email">Email : </label> <br>
+                <input type="email" name="email" id="email" placeholder="Email"> <br>
+
                 <label for="password">Password : </label> <br>
-                <input type="password" name="password" id="password" placeholder="Password"> <br>
+                <input type="password" name="pass" id="pass" placeholder="Password"> <br>
+
+                <label for="cpassword">Confirm Password : </label> <br>
+                <input type="password" name="cpass" id="cpass" placeholder="Confirm Password"> <br>
             
                 <button type="submit" class="login-btn" name="reg">
-                   <i class="fas fa-user-alt"></i> &nbsp;&nbsp; login
+                   <i class="fas fa-user-alt"></i> &nbsp;&nbsp; Create Account
                 </button>
             </form>
-            Already have an Account ? <a href="../../index.php">Create One</a>
+            Already have an Account ? <a href="../../index.php">Login</a>
         </div>
         <hr>
     </div>
