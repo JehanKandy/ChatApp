6fe3016cc9dba4414982f87e26192427bfc97a79
JehanKandy <jehankandy@gmail.com>
2023-04-05 11:08:53 +0530

    Update function.php

--- lib/function/function.php
+++ lib/function/function.php
@@ -1,5 +1,13 @@
 <?php
+    include("config.php");
 
+    use FTP\Connection;
+
+    session_start();
+
+    function reg_user($username, $email, $pass, $cpass){
+        $con = Connection();
+    }
 
 
 ?>
