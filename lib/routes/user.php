c0f0ca9d8e545ea5911ac14ea433c84f694009bc
JehanKandy <jehankandy@gmail.com>
2023-04-06 06:35:17 +0530

    Update user.php

--- lib/routes/user.php
+++ lib/routes/user.php
@@ -1,7 +1,9 @@
 <?php include("../layouts/header.php") ?>
 <link rel="stylesheet" href="../../css/style.css">
 
-
+<div class="main-content">
+    hi all
+</div>
 
 <script src="../../js/style.js"></script>
 
