9edbc73b88aedeb9c3cdf8c142b36fc12945f1ab
JehanKandy <jehankandy@gmail.com>
2023-04-06 07:59:08 +0530

    Update

---
+++ lib/layouts/loged_footer.php
@@ -0,0 +1,7 @@
+<link rel="stylesheet" href="../../css/style.css">
+<footer>
+    <div class="footer-content">
+        Developed By <a href="https://github.com/JehanKandy" target="_blank">JEHANKANDY</a>&copy; 2021-2023 all rights reserved
+    </div>
+</footer>
+<script src="../../js/style.js"></script>
--- lib/routes/user.php
+++ lib/routes/user.php
@@ -13,11 +13,9 @@
         </div>
     </div>
 </div>
-<footer>
-    <div class="footer-content">
-        Developed By <a href="https://github.com/JehanKandy" target="_blank">JEHANKANDY</a>&copy; 2021-2023 all rights reserved
-    </div>
-</footer>
+
+
+<?php include("../layouts/loged_footer.php");?>
 <script src="../../js/style.js"></script>
 
 
