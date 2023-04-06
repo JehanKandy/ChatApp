e1c511935a00c71975aede7c1ef83c27d0373f3e
JehanKandy <jehankandy@gmail.com>
2023-04-06 08:23:06 +0530

    Update user.php

--- lib/routes/user.php
+++ lib/routes/user.php
@@ -1,6 +1,16 @@
 <?php include("../layouts/header.php") ?>
 <link rel="stylesheet" href="../../css/style.css">
 
+<?php 
+    include("../function/function.php");
+
+    if(empty()){
+        
+    }
+
+?>
+
+
 <div class="user-content">
     <div class="container">
         <a href="../views/logout.php"><button class="logout-btn"><i class="fas fa-sign-out-alt"></i> Logout</button></a>
