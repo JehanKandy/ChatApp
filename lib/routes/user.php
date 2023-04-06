f9f0dda88779b2632fd5e79e58eafd846a19c91b
JehanKandy <jehankandy@gmail.com>
2023-04-06 07:14:22 +0530

    Update user.php

--- lib/routes/user.php
+++ lib/routes/user.php
@@ -2,7 +2,11 @@
 <link rel="stylesheet" href="../../css/style.css">
 
 <div class="user-content">
-    
+    <div class="container">
+        <div class="info">
+            hi
+        </div>
+    </div>
 </div>
 
 <script src="../../js/style.js"></script>
