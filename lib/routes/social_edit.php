e3302be8d9f869e5c80c5171ae9dbf0f62568c1c
JehanKandy <jehankandy@gmail.com>
2023-04-13 19:51:41 +0530

    Update

--- lib/routes/social_edit.php
+++ lib/routes/social_edit.php
@@ -10,10 +10,21 @@
                 <div class="row">
                     <div class="col-md-6">
                         Facebook :  <br>
-                        <input type="text" class="dash-input" value="Jehan" >
+                        <input type="text" class="dash-input" name="facebook"> <br><br>                    
                     </div>
                     <div class="col-md-6">
-
+                        Whatsapp :  <br>
+                        <input type="text" class="dash-input" name="whatsapp"> <br><br>     
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        Linkedin :  <br>
+                        <input type="text" class="dash-input" name="linkedin"> <br><br>                    
+                    </div>
+                    <div class="col-md-6">
+                        GitHub :  <br>
+                        <input type="text" class="dash-input" name="gitHub"> <br><br>     
                     </div>
                 </div>
             </div>
@@ -25,3 +36,4 @@
 
 
 <script src="../../js/style.js"></script>
+
