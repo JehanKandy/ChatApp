9e2cfc915749451ca6e039514dad97f1755a874c
JehanKandy <jehankandy@gmail.com>
2023-04-13 07:14:35 +0530

    Update

--- lib/routes/user.php
+++ lib/routes/user.php
@@ -24,10 +24,16 @@
         </div>
 
         <div class="main-grid">
-            <div class="row gx-2">
-                <div class="col-md-4" style="background:red;">hi</div>
-                <div class="col-md-8" style="background:green;">hi</div>
+            
+            <div class="row gx-3">
+                <div class="col">
+                <div class="p-3" style="background:red;">Custom column padding</div>
+                </div>
+                <div class="col">
+                <div class="p-3" style="background:green;">Custom column padding</div>
+                </div>
             </div>
+            
         </div>
 
         <?php include("../layouts/loged_footer.php");?>
