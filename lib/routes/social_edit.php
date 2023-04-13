58f6f9e302ba7026c720fea16b7606e36f35c4c5
JehanKandy <jehankandy@gmail.com>
2023-04-13 19:38:54 +0530

    Update

--- lib/routes/social_edit.php
+++ lib/routes/social_edit.php
@@ -7,7 +7,7 @@
             <a href="user.php"><button class="bck-btn">Back</button></a>
 
             <div class="social-media">
-                
+                Hi all
             </div>
 
         </div>
