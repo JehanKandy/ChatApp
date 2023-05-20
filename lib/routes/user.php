fc202521a03b067a314d47e864726231a1ae3179
JehanKandy <jehankandy@gmail.com>
2023-05-20 07:29:03 +0530

    Update

--- lib/routes/user.php
+++ lib/routes/user.php
@@ -33,7 +33,7 @@
                             <span style="color:#3b5998;"><i class="fab fa-facebook"></i> Facebook </span> <a href="<?php view_facebook(); ?>" target="_blank"><?php view_username(); ?>.facebook</a><br>
                             <span style="color:#25D366;"><i class="fab fa-whatsapp"></i> Whatsapp </span>  <a href="<?php view_whats(); ?>" target="_blank"><?php view_username(); ?>.Whatsapp</a><br>
                             <span style="color:#0072b1;"><i class="fab fa-linkedin"></i> Linkedin </span> <a href="<?php view_linkedin(); ?>" target="_blank"><?php view_username(); ?>.Linkedin</a><br>
-                            <span style="color:#171515;"><i class="fab fa-github"></i> GitHub </span> <a href="<?php view_linkedin(); ?>" target="_blank"><?php view_username(); ?>.Linkedin</a><br>
+                            <span style="color:#171515;"><i class="fab fa-github"></i> GitHub </span> <a href="<?php view_github(); ?>" target="_blank"><?php view_username(); ?>.GitHub</a><br>
                             <span style="color:#E1306C;"><i class="fab fa-instagram"></i> Instagram </span> <a href="<?php view_inster(); ?>" target="_blank"><?php view_username(); ?>.Instagram</a><br>
 
                             <a href="social_edit.php"><button class="social-btn">Update Information</button></a>
