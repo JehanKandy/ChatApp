015072e6e06d8857595a2fc4f29197addb2e5534
JehanKandy <jehankandy@gmail.com>
2023-05-20 07:39:21 +0530

    Update

--- lib/routes/user_data_update.php
+++ lib/routes/user_data_update.php
@@ -10,6 +10,53 @@
 
 ?>
 
+<div class="col">
+                    <div class="p-3 user-info">
+                        <div class="title">My Info</div>                        
+                        <div class="body">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <span>Username : </span>  <input type="text" class="dash-input" value="" disabled><br><br>
+                                </div>
+                                <div class="col-md-6">
+                                   <span>First Name : </span>  <input type="text" class="dash-input" value="" disabled><br><br>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <span>Email : </span>  <input type="email" class="dash-input" value="" disabled><br><br>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <span>Last Name : </span>  <input type="text" class="dash-input" value="" disabled><br><br>
+                                </div>
+                                <div class="col-md-6">
+                                   <span>Role : </span>  <input type="text" class="dash-input" value="" disabled><br><br>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <span>Address : </span>  <input type="text" class="dash-input" value="" disabled><br><br>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <span>Mobile Number : </span>  <input type="text" class="dash-input" value="" disabled><br><br>
+                                </div>
+                                <div class="col-md-6">
+                                   <span>Data of Birth : </span>  <input type="text" class="dash-input" value="" disabled><br><br>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <a href=""><button class="user-info-btn">Update Information</button></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
 
 
 <script src="../../js/style.js"></script>
